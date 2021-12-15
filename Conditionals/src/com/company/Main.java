39df975bc582fc3e7554ab44dd82b18bf188e2b7
thirteenpylons <thirteenpylons@gmail.com>
2021-12-14 20:14:19 -0500

    m

--- Conditionals/src/com/company/Main.java
+++ Conditionals/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean isAlien = false;
+        if (!isAlien) {
+            System.out.println("It is not an alien");
+            int topScore = 100;
+            if (topScore == 100) {
+                System.out.printf("You got the high score!");
+            }
+            int secondTopScore = 60;
+            if (topScore > secondTopScore && topScore < 100) {
+                System.out.println("Greater than second top score and less than 100.");
+            }
+        }
     }
 }
