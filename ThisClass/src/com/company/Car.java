3f801144168eaf30271f85ee6d1941074a13ddcc
thirteenpylons <thirteenpylons@gmail.com>
2021-12-16 07:37:46 -0500

    classes

---
+++ ThisClass/src/com/company/Car.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Locale;
+
+/**
+*
+ */
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    // Method that will update the model
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")){
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+}
